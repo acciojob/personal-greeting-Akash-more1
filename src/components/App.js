b125d8b3d97ebbee0bec7b7a710063dbfcc34752
Akash-more1 <akashmore9725@gmail.com>
2024-07-22 18:56:59 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [greet, setGreet]=useState("");
+
+  const greeting= (e) =>{
+    setGreet("Hello "+e.target.value+"!");
+  }
   return (
     <div>
+      <input type="text" onChange={greeting}></input>
+      <h1> {greet}</h1>
         {/* Do not remove the main div */}
     </div>
   )
